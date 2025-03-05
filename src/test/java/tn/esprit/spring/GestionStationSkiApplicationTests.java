be013dd83ef4e94fb2a294225b27a335a6439cc1
Manai Yassine <98694575+YassineManai@users.noreply.github.com>
2025-03-05 23:17:44 +0100

    test run local

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,16 +2,12 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // Use the "test" profile
-class GestionStationSkiApplicationTests {
+public class GestionStationSkiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// This test verifies that the Spring application context loads successfully.
-		// No additional assertions are needed as the test will fail if the context fails to load.
+		// This test will fail if the application context cannot be loaded
 	}
-
 }
