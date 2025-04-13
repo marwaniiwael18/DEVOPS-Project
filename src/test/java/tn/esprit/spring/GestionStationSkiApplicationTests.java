3db03d853bb524cc254bd761edfb5d81a01c08a8
Manai Yassine <98694575+YassineManai@users.noreply.github.com>
2025-04-13 22:25:23 +0100

    lets try this

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,6 +2,8 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 public class GestionStationSkiApplicationTests {
